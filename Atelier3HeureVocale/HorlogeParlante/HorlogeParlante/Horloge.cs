b211435ad5dc8e51666b29cae9be7de28e74f59f
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-05-22 19:20:40 +0200

    atelier3: refactor - extrait le tableau des noms d'heures en champ statique

--- Atelier3HeureVocale/HorlogeParlante/HorlogeParlante/Horloge.cs
+++ Atelier3HeureVocale/HorlogeParlante/HorlogeParlante/Horloge.cs
@@ -2,13 +2,14 @@ namespace HorlogeParlante;
 
 public static class Horloge
 {
+    private static readonly string[] NomsHeures =
+        { "", "une", "deux", "trois", "quatre", "cinq", "six",
+          "sept", "huit", "neuf", "dix", "onze" };
+
     public static string EnTexte(DateTime heure)
     {
-        string[] noms = { "", "une", "deux", "trois", "quatre", "cinq", "six",
-                           "sept", "huit", "neuf", "dix", "onze" };
-
         int h = heure.Hour;
-        string motHeure = h == 1 ? "une heure" : $"{noms[h]} heures";
+        string motHeure = h == 1 ? "une heure" : $"{NomsHeures[h]} heures";
         return $"{motHeure} du matin";
     }
 }
