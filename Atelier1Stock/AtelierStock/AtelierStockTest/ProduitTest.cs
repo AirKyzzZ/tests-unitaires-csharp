167fd54ff7e815ac49971871b3dfcd8006847ab4
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-05-22 19:08:49 +0200

    atelier1: code de départ (classe Produit + squelette de test fourni)

---
+++ Atelier1Stock/AtelierStock/AtelierStockTest/ProduitTest.cs
@@ -0,0 +1,43 @@
+namespace AtelierStock
+{
+    [TestClass]
+    public class ProduitTest
+    {
+        [TestMethod]
+        public void Initialiser_ProduitQuelconque()
+        {
+            var prixAchat = 100.0m;
+            var marge = 0.18m;
+
+            var p = new Produit("REF", "UnNom", prixAchat, marge);
+
+            Assert.AreEqual("REF"  , p.Reference);
+            Assert.AreEqual("UnNom", p.Libelle);
+            Assert.AreEqual(100.0m , p.PrixAchat);
+            Assert.AreEqual(118.0m , p.PrixVente);
+            Assert.AreEqual(0, p.Stocks);
+            Assert.IsTrue(p.EstEnRupture);
+        }
+        [TestMethod]
+        public void Initialiser_ProduitMarge0()
+        {
+            var p = new Produit("REF", "UnNom", 100, 0);
+
+            Assert.AreEqual("REF", p.Reference);
+            Assert.AreEqual("UnNom", p.Libelle);
+            Assert.AreEqual(100.0m, p.PrixAchat);
+            Assert.AreEqual(100.0m, p.PrixVente);
+            Assert.AreEqual(0, p.Stocks);
+            Assert.IsTrue(p.EstEnRupture);
+        }
+
+        [TestMethod]
+        public void Initialiser_ProduitReferenceVide_LeveArgumentException()
+        {
+            Action act = () => new Produit("", "UnNom", 100, 0);
+
+            Assert.Throws<ArgumentException>(act);
+        }
+
+    }
+}
