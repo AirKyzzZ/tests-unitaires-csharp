35c64407492857572574a0524926958afa2cd11d
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-05-22 19:31:11 +0200

    atelier4: code de départ (classes Gladiator et Dice fournies par l'énoncé)

---
+++ Atelier4Arene/Arene/Arene/Gladiator.cs
@@ -0,0 +1,16 @@
+namespace Arene;
+
+public class Gladiator(string name, int health, int strength, int armor)
+{
+    public string Name { get; } = name;
+    public int Health { get; private set; } = health;
+    public int Strength { get; private set; } = strength;
+    public int Armor { get; private set; } = armor;
+
+    public void Attack(Gladiator opponent, Dice dice)
+    {
+        var score = dice.Roll();
+
+        throw new NotImplementedException("To be done");
+    }
+}
