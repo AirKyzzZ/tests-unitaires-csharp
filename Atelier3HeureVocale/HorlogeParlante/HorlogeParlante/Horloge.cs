68ea430885612e43471055704fc750859791e72d
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-05-22 19:19:48 +0200

    atelier3: green - heure pile du matin

--- Atelier3HeureVocale/HorlogeParlante/HorlogeParlante/Horloge.cs
+++ Atelier3HeureVocale/HorlogeParlante/HorlogeParlante/Horloge.cs
@@ -3,5 +3,12 @@ namespace HorlogeParlante;
 public static class Horloge
 {
     public static string EnTexte(DateTime heure)
-        => throw new NotImplementedException();
+    {
+        string[] noms = { "", "une", "deux", "trois", "quatre", "cinq", "six",
+                           "sept", "huit", "neuf", "dix", "onze" };
+
+        int h = heure.Hour;
+        string motHeure = h == 1 ? "une heure" : $"{noms[h]} heures";
+        return $"{motHeure} du matin";
+    }
 }
