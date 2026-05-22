d50d75b93889c01afb43125ca71a908fe556a10d
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-05-22 19:34:02 +0200

    atelier4: green - le dé truqué hors de 1..6 est rejeté

--- Atelier4Arene/Arene/Arene/Gladiator.cs
+++ Atelier4Arene/Arene/Arene/Gladiator.cs
@@ -9,7 +9,12 @@ public class Gladiator(string name, int health, int strength, int armor)
 
     public void Attack(Gladiator opponent, IDice dice)
     {
-        var score = dice.Roll();
+        int score = dice.Roll();
+        if (score is < 1 or > 6)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(dice), score, "Le dé doit produire une valeur entre 1 et 6.");
+        }
 
         throw new NotImplementedException("To be done");
     }
